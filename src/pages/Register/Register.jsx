b7800aace264622bd99f2c0fd8f184811bb7a8f0
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2024-10-12 13:13:38 -0800

    coding dynamic sections

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { FaGoogle } from "react-icons/fa";
+import swal from "sweetalert";
+import { formValidation } from "../../utils/Validations";
+import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Register() {
+    const baseUrl = process.env.REACT_APP_BASE_URL
+    const navigate = useNavigate();
+
+
+    const form = useForm();
+    const { register, control, handleSubmit, formState, reset } = form
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        let formData = new FormData()
+        formData.append('name', data.name)
+        formData.append('phone', data.phone)
+
+
+        fetch(`${baseUrl}register`,
+            {
+                method: 'POST',
+                body: formData
+            })
+            .then(res => res.json())
+            .then(response => {
+                if (response.status !== false) {
+                    localStorage.setItem('phone', JSON.stringify( response.phone ))
+                    swal({
+                        title: response.message[0],
+                        icon: "success",
+                        buttons: 'باشه'
+                    }).then(response => {
+                        reset();
+                        navigate('/verify-phone-number');
+                    })
+                } else {
+                    swal({
+                        title: response.message[0],
+                        icon: "error",
+                        buttons: 'باشه'
+                    })
+                }
+
+            })
+    }
+    return (
+        <>
+            <Container fluid>
+                <Row>
+                    <Col lg={5} className='p-2 h-100vh'>
+                        <div className='letf-login-side h-100'>
+                            <div className=''>
+                                <img src="images/banner/Sport Illustration Kit-10.png" className='w-100' alt="" />
+                                <div className='text-center l-r-text mt-2'>
+                                    ساخت حساب کاربری
+                                </div>
+                                <div className='mt-1 text-center text-secondary fs15 px-4 lh2'>
+                                    با ثبت نام امکان خرید برنامه های تمرینی و دوره های ویدیویی برای شما فراهم میشود
+                                    ابتدا شماره موبایل خود را وارد نمایید و در صفحه بعد آن را تایید کنید.
+                                </div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col lg={7} className='h-100vh'>
+                        <Row className='justify-content-center h-100 align-items-center'>
+                            <Col lg={6}>
+                                <div className='fw-bold text-center fs30'>
+                                    به سایت <span className='color-1'>مربی همراه </span> خوش آمدید
+                                </div>
+                                <form onSubmit={handleSubmit(onSubmit)} noValidate>
+
+
+                                    <div className='fs14 mt-5'>سلام!</div>
+                                    <div className='fs14 mt-3'>لطفا اطلاعات خود را وارد نمایید</div>
+                                    <input type="text" className='w-100 custom-input px-3 mt-4' placeholder='نام و نام خانوادگی'
+                                        {...register('name', formValidation('نام'))}
+                                    />
+                                    <p className='mt-2 text-danger px-2 fs13'>
+                                        {errors.name?.message}
+                                    </p>
+                                    <input type="text" className='w-100 custom-input px-3' placeholder='شماره موبایل'
+                                        {...register('phone', formValidation('شماره موبایل'))}
+                                    />
+                                    <p className='mt-2 text-danger px-2 fs13'>
+                                        {errors.phone?.message}
+                                    </p>
+                                    <button className='login-btn mt-1 w-100 text-white'>ثبت نام</button>
+                                </form>
+                            </Col>
+                        </Row>
+                        <div className='ball ball-1'></div>
+                        <div className='ball ball-2'></div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -2,11 +2,54 @@ import React from 'react'
 import './style.css';
 import { Col, Container, Row } from 'react-bootstrap';
 import { TbEditCircle } from "react-icons/tb";
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation } from 'react-router-dom';
+import swal from "sweetalert";
+import { formValidation } from "../../utils/Validations";
+import { useForm } from "react-hook-form";
 
 
 export default function Verify() {
+    const baseUrl = process.env.REACT_APP_BASE_URL
+    const phoneNumber = JSON.parse(localStorage.getItem('phone'))
+
+
+
+    const form = useForm();
+    const { register, control, handleSubmit, formState, reset } = form
+    const { errors } = formState;
+
+    const onSubmit = (data) => {
+        let formData = new FormData()
+        formData.append('code', data.code)
+        formData.append('phone', phoneNumber)
+
+
+        fetch(`${baseUrl}verify-phone-number`,
+            {
+                method: 'POST',
+                body: formData
+            })
+            .then(res => res.json())
+            .then(response => {
+                if (response.status !== false) {
+                    localStorage.setItem('phone', JSON.stringify(response.phone))
+                    swal({
+                        title: response.message[0],
+                        icon: "success",
+                        buttons: 'باشه'
+                    }).then(response => {
+                        reset();
+                    })
+                } else {
+                    swal({
+                        title: response.message[0],
+                        icon: "error",
+                        buttons: 'باشه'
+                    })
+                }
+
+            })
+    }
     return (
         <>
             <Container fluid>
@@ -31,25 +74,31 @@ export default function Verify() {
                                     تایید  <span className='color-1'>شماره موبایل</span>
                                 </div>
                                 <div className='text-center text-secondary fs14 mt-4 pt-2'>
-                                    کد تائید برای شماره موبایل ۰۹۳۰۹۵۱۹۳۶۶ ارسال گردید
+                                    کد تائید برای شماره موبایل <span className='ffv'>{phoneNumber}</span> ارسال گردید
                                     <Link>
                                         <TbEditCircle className='me-2 text-secondary' fontSize={19} />
                                     </Link>
                                 </div>
-
+                                {/* 
                                 <div className='text-center mt-4 pt-3 fw-bold fs14'>
                                     کد تایید را وارد نمایید
-                                </div>
-                                <div className='mt-4 text-center'>
+                                </div> */}
+                                <div className='mt-3 text-center'>
+                                    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+
+                                        {/* <input type="text" className='input-for-verify-code mx-1' />
                                     <input type="text" className='input-for-verify-code mx-1' />
                                     <input type="text" className='input-for-verify-code mx-1' />
                                     <input type="text" className='input-for-verify-code mx-1' />
                                     <input type="text" className='input-for-verify-code mx-1' />
-                                    <input type="text" className='input-for-verify-code mx-1' />
-                                    <input type="text" className='input-for-verify-code mx-1' />
+                                    <input type="text" className='input-for-verify-code mx-1' /> */}
+                                        <input type="text" className='w-100 custom-input text-center px-3 mt-4' placeholder='کد تایید را وارد نمایید'
+                                        {...register('code', formValidation('کد'))}
+                                        />
+                                    </form>
                                 </div>
                                 <div className='text-center'>
-                                    <button class="login-btn mt-4 fs15 w-100 text-white mt-5">تایید و ادامه</button>
+                                    <button class="login-btn mt-4 fs15 w-100 text-white mt-4">تایید و ادامه</button>
                                 </div>
                             </Col>
 
